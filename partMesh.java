6c88eb23a262ce578cace4ec6852740df92dcf61
Changhoon <30878360+hoonjet@users.noreply.github.com>
2019-10-08 15:07:34 +0900

    partMesh.java // generates mesh (part mesh)

---
+++ partMesh.java
@@ -0,0 +1,40 @@
+// Generates mesh and save the file - part mesh 
+
+package macro;
+
+import java.util.*;
+import star.common.*;
+import star.base.neo.*;
+import star.meshing.*;
+
+public class partMesh extends StarMacro {
+
+  public void execute() {
+    meshing();
+  }
+
+  private void meshing() {
+	  
+  	/*get active simulation */
+	Simulation simulation_0 = getActiveSimulation();
+    
+	/* Path and file name */
+	String name = getActiveSimulation().getPresentationName();		
+	String fullPath = resolvePath(name);				
+	String filePath = fullPath.substring(0,fullPath.length()-name.length());
+	String fileName = name;
+
+    /* mesh and save simulation */    	
+	AutoMeshOperation autoMeshOperation_0 = ((AutoMeshOperation) simulation_0.get(MeshOperationManager.class).getObject("Automated Mesh"));	
+    autoMeshOperation_0.executeSurfaceMeshers(); 
+    autoMeshOperation_0.execute();
+
+	/* save and close simulation */
+	simulation_0.saveState(name+"_mesh.sim");
+
+	/* close simulation */	
+	simulation_0.kill();
+
+  }
+}
+
