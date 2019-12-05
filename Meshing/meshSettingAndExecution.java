03d1056967d174ab275dfdb861c3d4fef89c8200
Changhoon <30878360+hoonjet@users.noreply.github.com>
2019-12-05 14:39:56 +0900

    Update meshSettingAndExecution.java

--- Meshing/meshSettingAndExecution.java
+++ Meshing/meshSettingAndExecution.java
@@ -26,54 +26,53 @@ public class meshSettingAndExecution extends StarMacro {
     
     private void meshSettings() {
 
-		// Get active simulation
+	// Get active simulation
         Simulation simulation_0 = getActiveSimulation();
         String dir = simulation_0.getSessionDir();
-		String name = simulation_0.getPresentationName();	
+	String name = simulation_0.getPresentationName();	
         String sep = System.getProperty("file.separator");
         
         // get Parts(assembly) for meshing
         GeometryPart composite_Manifold = simulation_0.get(SimulationPartManager.class).getPart("Manifold");
 
         // create Automated Mesh Operation
-		/*  Options
-			1) Surface remesher - Automatic surface Repair
-			2) Polyhedral Mesher Mesher
-			3) Thin Mesher (Solid only)
-			4) Prism layer Mesher (Fluid only) */
+	/*  Options (Selected Meshers)
+	  1) Surface remesher - Automatic surface Repair
+	  2) Polyhedral Mesher Mesher
+	  3) Thin Mesher (Solid only)
+	  4) Prism layer Mesher (Fluid only) 
+	*/
         AutoMeshOperation autoMeshOperation = simulation_0.get(MeshOperationManager.class).createAutoMeshOperation(new StringVector(new String[]{"star.resurfacer.ResurfacerAutoMesher","star.dualmesher.DualAutoMesher", "star.solidmesher.ThinAutoMesher", "star.prismmesher.PrismAutoMesher"}), Arrays.asList(composite_Manifold));
 		
-		// set units to mm
+	// set units to mm
         Units units_mm = simulation_0.getUnitsManager().getObject("mm");
 
-		// Mesh settings
-			//1) Base size = 100.0 mm
-			double baseSize = 100.0;
-			autoMeshOperation.getDefaultValues().get(BaseSize.class).setUnits(units_mm);
-			autoMeshOperation.getDefaultValues().get(BaseSize.class).setValue(baseSize);
-			
-			//2) Target surface size: 10% of base size
-			double targetSizePct = 10.0;
-			autoMeshOperation.getDefaultValues().get(PartsTargetSurfaceSize.class).getRelativeSizeScalar().setValue(targetSizePct);			
-			//3) Minimum surface size: 1% of base size
-			double minimumSizePct = 1.0;
-			autoMeshOperation.getDefaultValues().get(PartsMinimumSurfaceSize.class).getRelativeSizeScalar().setValue(minimumSizePct);			
-			//4) number of thin layers = 5
-			int numThinLayers = 5;
-			autoMeshOperation.getDefaultValues().get(ThinNumLayers.class).setLayers(numThinLayers);
-			// 5) Prism Layer absolute size = 2.0 mm
-			double prismLayerSize = 2.0;
-			autoMeshOperation.getDefaultValues().get(PrismThickness.class).setAbsoluteSize(prismLayerSize, units_mm);
+	// Mesh settings
+		//1) Base size = 100.0 mm
+		double baseSize = 100.0;
+		autoMeshOperation.getDefaultValues().get(BaseSize.class).setUnits(units_mm);
+		autoMeshOperation.getDefaultValues().get(BaseSize.class).setValue(baseSize);	
+		//2) Target surface size: 10% of base size
+		double targetSizePct = 10.0;
+		autoMeshOperation.getDefaultValues().get(PartsTargetSurfaceSize.class).getRelativeSizeScalar().setValue(targetSizePct);			
+		//3) Minimum surface size: 1% of base size
+		double minimumSizePct = 1.0;
+		autoMeshOperation.getDefaultValues().get(PartsMinimumSurfaceSize.class).getRelativeSizeScalar().setValue(minimumSizePct);			
+		//4) number of thin layers = 5
+		int numThinLayers = 5;
+		autoMeshOperation.getDefaultValues().get(ThinNumLayers.class).setLayers(numThinLayers);
+		// 5) Prism Layer absolute size = 2.0 mm
+		double prismLayerSize = 2.0;
+		autoMeshOperation.getDefaultValues().get(PrismThickness.class).setAbsoluteSize(prismLayerSize, units_mm);
         
-		// Mesh custom control - disable thin mesher in fluid region, while disable prism layers in solid region
+	// Mesh custom control - disable thin mesher in fluid region, while disable prism layers in solid region
         meshCustomControl(autoMeshOperation);
-	
 		
 	}
 	
 	private void meshCustomControl(AutoMeshOperation meshOperation) {
       
-	    // disable proximity refinement
+	// disable proximity refinement
         meshOperation.getMeshers().hasMesher(ResurfacerAutoMesher.class).setDoProximityRefinement(false);
 
         // disable prism mesh in solids 
@@ -95,16 +94,16 @@ public class meshSettingAndExecution extends StarMacro {
 	
 	public void generateMesh(){
 		
-		// Get active simulation
+	// Get active simulation
         Simulation simulation_0 = getActiveSimulation();
         String dir = simulation_0.getSessionDir();
-		String name = simulation_0.getPresentationName();	
+	String name = simulation_0.getPresentationName();	
         String sep = System.getProperty("file.separator");
 
         // start meshing pipeline
        simulation_0.get(MeshPipelineController.class).generateVolumeMesh();
        
-		// Save data file
+	// Save data file
        simulation_0.saveState(resolvePath(name) + "_meshFinished.sim");
     }
        
