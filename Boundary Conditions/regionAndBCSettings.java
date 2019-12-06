d58e4a8e0062097e23abad76de5cfee5be70f535
Changhoon <30878360+hoonjet@users.noreply.github.com>
2019-12-06 11:18:05 +0900

    Update regionAndBCSettings.java

--- Boundary Conditions/regionAndBCSettings.java
+++ Boundary Conditions/regionAndBCSettings.java
@@ -29,45 +29,45 @@ public class regionAndBCSettings extends StarMacro {
 
     public void execute() {
 		
-		// Load a file & get basic properties 
-		Simulation sim = getActiveSimulation();			
-		String name = getActiveSimulation().getPresentationName();		
+	// Load a file & get basic properties 
+	Simulation sim = getActiveSimulation();			
+	String name = getActiveSimulation().getPresentationName();		
 
-		// Set up region and physics
-		setUpRegions(sim);
+	// Set up region and physics
+	setUpRegions(sim);
 		
-		// set up boundary conditions
-		setUpBoundaryConditions(sim);
+	// set up boundary conditions
+	setUpBoundaryConditions(sim);
 		
-		// Save a file	
-		sim.saveState(resolvePath(name) + "_setUpFinished.sim");
+	// Save a file	
+	sim.saveState(resolvePath(name) + "_setUpFinished.sim");
 			
 	}
 		
 	public void setUpRegions(Simulation sim){
 				
 		
-		// Get Regions & continua, 
+	// Get Regions & continua, 
         Region fluidRegion = sim.getRegionManager().getRegion("Manifold.Fluid");
         Region solidRegion = sim.getRegionManager().getRegion("Manifold.Solid");
-		PhysicsContinuum fluidPhys = ((PhysicsContinuum) sim.getContinuumManager().getContinuum("Fluid"));
-		PhysicsContinuum solidPhys = ((PhysicsContinuum) sim.getContinuumManager().getContinuum("Solid"));				
+	PhysicsContinuum fluidPhys = ((PhysicsContinuum) sim.getContinuumManager().getContinuum("Fluid"));
+	PhysicsContinuum solidPhys = ((PhysicsContinuum) sim.getContinuumManager().getContinuum("Solid"));				
 		
-		// Assign each continuum to correponding regions
-		fluidPhys.add(fluidRegion);
-		solidPhys.add(solidRegion);
+	// Assign each continuum to correponding regions
+	fluidPhys.add(fluidRegion);
+	solidPhys.add(solidRegion);
 		
 	}
 	
 	public void setUpBoundaryConditions(Simulation sim){
 		
-		// Get Regions to set boundary conditions
-		Region fluidRegion = sim.getRegionManager().getRegion("Manifold.Fluid");
+	// Get Regions to set boundary conditions
+	Region fluidRegion = sim.getRegionManager().getRegion("Manifold.Fluid");
         Region solidRegion = sim.getRegionManager().getRegion("Manifold.Solid");
 
-		// Change boundary type according to the name (use Query)
-		// boundary will be set to "velocity inlet" if it has "inlet" in its name, "pressure outlet" if it has "outlet".
-		// All the others will be set to wall by default
+	// Change boundary type according to the name (use Query)
+	// boundary will be set to "velocity inlet" if it has "inlet" in its name, "pressure outlet" if it has "outlet".
+	// All the others will be set to wall by default
         Collection<Region> regions = sim.getRegionManager().getRegions();
         for (Region region : regions) {
             Collection<Boundary> boundaries = region.getBoundaryManager().getBoundaries();
@@ -80,31 +80,30 @@ public class regionAndBCSettings extends StarMacro {
             }
         }		
 	
-		// Set up BCs for the fluid     
-		/* To Do:
-			1) Set up inlet 1 temperature 500 K, inlet magnitude 2.0 m/s
-			2) Other inlets have temperature 350K, inlet velocity of 0.1 m/s */
-		Collection<Boundary> fluidBoundaries = sim.getRegionManager().getRegion("Manifold.Fluid").getBoundaryManager().getBoundaries();
-		for (Boundary boundary : fluidBoundaries) {
+	// Set up BCs for the fluid     
+	/* To Do:
+	   1) Set up inlet 1 temperature 500 K, inlet magnitude 2.0 m/s
+	   2) Other inlets have temperature 350K, inlet velocity of 0.1 m/s */
+	Collection<Boundary> fluidBoundaries = sim.getRegionManager().getRegion("Manifold.Fluid").getBoundaryManager().getBoundaries();
+	for (Boundary boundary : fluidBoundaries) {
             String boundaryName = boundary.getPresentationName();
             if (boundaryName.contains("Inlet")){
-				if (!boundaryName.equalsIgnoreCase("Inlet 1")) {
-                boundary.getValues().get(VelocityMagnitudeProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(0.1);
-                boundary.getValues().get(StaticTemperatureProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(350.0);
-				} 
-				else {
-				boundary.getValues().get(VelocityMagnitudeProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(2.0); 
-				boundary.getValues().get(StaticTemperatureProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(500.0);		
-				}
+		if (!boundaryName.equalsIgnoreCase("Inlet 1")) {
+        	        boundary.getValues().get(VelocityMagnitudeProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(0.1);
+                	boundary.getValues().get(StaticTemperatureProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(350.0);
+		} 
+		else {
+			boundary.getValues().get(VelocityMagnitudeProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(2.0); 
+			boundary.getValues().get(StaticTemperatureProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(500.0);		
+		}
             }
         }       
 		
         // Set up BCs for the solid
-		// To Do: set wall thermal type to convection, with a heat transfer coeffcient of 50 W/m^2K
+	// To Do: set wall thermal type to convection, with a heat transfer coeffcient of 50 W/m^2K
         Boundary solidWall = solidRegion.getBoundaryManager().getBoundary("Wall");
         solidWall.getConditions().get(WallThermalOption.class).setSelected(WallThermalOption.Type.CONVECTION);
         solidWall.getValues().get(HeatTransferCoefficientProfile.class).getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(50.0);
-		
 	}
 }
 
